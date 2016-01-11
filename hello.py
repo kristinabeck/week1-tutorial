4f163d2a05f36fa20ab548cf6b505319fb42be2d
Kristina Beck <kbeck@Kristinas-MBP.iamunix.org>
2016-01-11 16:01:50 -0500

    step 4 of instructions

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 # replace the word 'hector' with your own name
 
 def hello_world():
-	return 'hello world, my name is ' 
+	return 'hello world, my name is Kristina ' 
 
-print( hello_world() + 'Kristina' )
+print( hello_world() + 'hector' )
